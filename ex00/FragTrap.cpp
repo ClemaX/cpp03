f006f4bbde592a73445f3271f0af1ce2d4ac3a62
Clément Hamada <clementhamada@protonmail.com>
2020-05-15 20:43:20 +0200

    ex00

---
+++ ex00/FragTrap.cpp
@@ -0,0 +1,83 @@
+#include "FragTrap.hpp"
+
+Attack::Attack(std::string name, int damage)
+	: name(name), damage(damage)
+{
+}
+
+Attack::Attack(std::string name, int damage, int cost)
+	: name(name), damage(damage), cost(cost)
+{
+}
+
+FragTrap::FragTrap(std::string name)
+	: name(name)
+{
+	std::cout << "FR4G-TP ";
+	std::cout << "Constructing " << name << '!';
+	std::cout << std::endl;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "FR4G-TP ";
+	std::cout << "Destructing " << name << '!';
+	std::cout << std::endl;
+}
+
+void performAttack(std::string &source, std::string const &target, Attack const &attack)
+{
+	std::cout << "FR4G-TP ";
+	std::cout << source << " attacks " << target << ' ';
+	std::cout << "with " << attack.name << ", ";
+	std::cout << "causing " << attack.damage << " points of damage!";
+	std::cout << std::endl;
+}
+
+void FragTrap::rangedAttack(std::string const &target)
+{
+	performAttack(name, target, ranged);
+}
+
+void FragTrap::meleeAttack(std::string const &target)
+{
+	performAttack(name, target, melee);
+}
+
+void FragTrap::takeDamage(unsigned int amount)
+{
+	amount -= armorReduction;
+	if (amount > hitPoints)
+		amount = hitPoints;
+	std::cout << "FR4G-TP ";
+	std::cout << '<' << name << "> ";
+	std::cout << "takes " << amount << " points of damage!";
+	std::cout << std::endl;
+	hitPoints -= amount;
+}
+
+void FragTrap::beRepaired(unsigned int amount)
+{
+	if (hitPoints + amount > maxHitPoints)
+		amount = maxHitPoints - hitPoints;
+	std::cout << "FR4G-TP ";
+	std::cout << name << " heals " << amount << " hit points!";
+	std::cout << std::endl;
+	hitPoints += amount;
+}
+
+void FragTrap::vaulthunter_dot_exe(std::string const &target)
+{
+	Attack &attack = specialAttacks[rand() % specialAttacks.size()];
+	if (attack.cost > energyPoints)
+	{
+		std::cout << "FR4G-TP ";
+		std::cout << name << " does not have enough energy to use " << attack.name << "!";
+		std::cout << std::endl;
+	}
+	else
+	{
+		energyPoints -= attack.cost;
+		performAttack(name, target, attack);
+	}
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,15 @@
+#include "FragTrap.hpp"
+
+int main(void)
+{
+	FragTrap a = FragTrap("Alpha");
+	FragTrap b = FragTrap("Beta");
+
+	a.meleeAttack(b.name);
+	b.rangedAttack(a.name);
+
+	for (int i = 0; i < a.specialAttacks.size(); i++)
+		a.vaulthunter_dot_exe(b.name);
+
+	return 0;
+}

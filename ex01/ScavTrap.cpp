1cfc9c125f2a55109173901dd650379f297117d0
Clément Hamada <clementhamada@protonmail.com>
2021-02-09 16:48:55 +0100

    ex01: ScavTrap: Apply damage and energyCost

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -15,12 +15,13 @@ const ScavTrap::Attack	ScavTrap::melee = ScavTrap::Attack("melee", 20);
 
 const ScavTrap::Attack	ScavTrap::ranged = ScavTrap::Attack("ranged", 15);
 
-ScavTrap::Attack::Attack(std::string name, unsigned int damage)
+ScavTrap::Attack::Attack(std::string const& name, unsigned int damage)
 	: name(name), damage(damage), cost(0)
 {
 }
 
-ScavTrap::Attack::Attack(std::string name, unsigned int damage, unsigned int cost)
+ScavTrap::Attack::Attack(std::string const& name, unsigned int damage,
+	unsigned int cost)
 	: name(name), damage(damage), cost(cost)
 {
 }
@@ -45,21 +46,33 @@ ScavTrap::~ScavTrap()
 	std::cout << std::endl;
 }
 
-void ScavTrap::performAttack(ScavTrap const& target, ScavTrap::Attack const &attack)
+void ScavTrap::performAttack(ScavTrap& target, ScavTrap::Attack const &attack)
 {
-	std::cout << "SC4V-TP ";
-	std::cout << name << " attacks " << target.name << ' ';
-	std::cout << "with " << attack.name << ", ";
-	std::cout << "causing " << attack.damage << " points of damage!";
-	std::cout << std::endl;
+	std::cout << "SC4V-TP " << name;
+	if (attack.cost > energyPoints)
+	{
+		std::cout << " does not have enough energy to use ";
+		std::cout << attack.name << "!";
+		std::cout << std::endl;
+	}
+	else
+	{
+		std::cout << " attacks " << target.name << ' ';
+		std::cout << "with " << attack.name << ", ";
+		std::cout << "causing " << attack.damage << " points of damage!";
+		std::cout << std::endl;
+
+		energyPoints -= attack.cost;
+		target.takeDamage(attack.damage);
+	}
 }
 
-void ScavTrap::rangedAttack(ScavTrap const& target)
+void ScavTrap::rangedAttack(ScavTrap& target)
 {
 	performAttack(target, ranged);
 }
 
-void ScavTrap::meleeAttack(ScavTrap const& target)
+void ScavTrap::meleeAttack(ScavTrap& target)
 {
 	performAttack(target, melee);
 }
@@ -69,10 +82,12 @@ void ScavTrap::takeDamage(unsigned int amount)
 	amount -= armorReduction;
 	if (amount > hitPoints)
 		amount = hitPoints;
+
 	std::cout << "SC4V-TP ";
-	std::cout << '<' << name << "> ";
-	std::cout << "takes " << amount << " points of damage!";
+	std::cout << name;
+	std::cout << " takes " << amount << " points of damage!";
 	std::cout << std::endl;
+
 	hitPoints -= amount;
 }
 
@@ -80,9 +95,11 @@ void ScavTrap::beRepaired(unsigned int amount)
 {
 	if (hitPoints + amount > maxHitPoints)
 		amount = maxHitPoints - hitPoints;
+
 	std::cout << "SC4V-TP ";
 	std::cout << name << " heals " << amount << " hit points!";
 	std::cout << std::endl;
+
 	hitPoints += amount;
 }
 

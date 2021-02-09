48ca4b3818b29a40466ca8f9615986d5996d5dd3
Clément Hamada <clementhamada@protonmail.com>
2021-02-09 16:47:58 +0100

    ex01: FragTrap: Use symbolic link

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,23 +3,37 @@
 
 int main(void)
 {
+	srand(time(NULL));
+
 	FragTrap a = FragTrap("Alpha");
 	FragTrap b = FragTrap("Beta");
 
 	ScavTrap c = ScavTrap("Charlie");
 	ScavTrap d = ScavTrap("Delta");
 
+	std::cout << std::endl;
+
 	a.meleeAttack(b);
 	b.rangedAttack(a);
+
+	std::cout << std::endl;
 
 	c.meleeAttack(d);
 	d.rangedAttack(c);
 
+	std::cout << std::endl;
+
+	a.beEnergized(4 * 25);
+
 	for (std::size_t i = 0; i < 5; i++)
 		a.vaulthunter_dot_exe(b);
 
+	std::cout << std::endl;
+
 	for (std::size_t i = 0; i < 5; i++)
 		c.challengeNewcomer();
+
+	std::cout << std::endl;
 
 	return 0;
 }

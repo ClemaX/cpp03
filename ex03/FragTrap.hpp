f6432eaf08f11827de8c6cc4cad38d99e77632d7
Clément Hamada <clementhamada@protonmail.com>
2021-02-09 13:05:56 +0100

    ex03: FragTrap: Update

--- ex03/FragTrap.hpp
+++ ex03/FragTrap.hpp
@@ -1,21 +1,53 @@
 #pragma once
 
-#include "ClapTrap.hpp"
+#include <string>
+#include <iostream>
+#include <array>
+#include <random>
 
-class FragTrap : public ClapTrap
+class FragTrap
 {
-public:
-	std::array<Attack, 5> specialAttacks = {
-		Attack("a pan", 5, 25),
-		Attack("his feet", 10, 25),
-		Attack("a magic wand", 10, 25),
-		Attack("his bare hands", 5, 25),
-		Attack("nuclear power", 1000, 25)
+protected:
+	unsigned int	level;
+
+	unsigned int	maxHitPoints;
+	unsigned int	maxEnergyPoints;
+	unsigned int	armorReduction;
+
+	unsigned int	hitPoints;
+	unsigned int	energyPoints;
+
+	class Attack
+	{
+	public:
+		const std::string	name;
+		const unsigned int	damage;
+		const unsigned int	cost;
+
+		Attack(std::string name, unsigned int damage);
+		Attack(std::string name, unsigned int damage, unsigned int energyCost);
 	};
 
-	FragTrap(std::string name);
+	static const Attack	melee;
+	static const Attack	ranged;
+	static const size_t	specialAttacksCount;
+	static const Attack	specialAttacks[];
+
+	void	performAttack(FragTrap const& target,
+		FragTrap::Attack const& attack);
+
+public:
+	const std::string	name;
+
+	FragTrap(std::string const& name);
 	~FragTrap();
 
-	void performAttack(std::string &source, std::string const &target, ClapTrap::Attack const &attack);
-	void vaulthunter_dot_exe(std::string const &target);
+	void	rangedAttack(FragTrap const& target);
+	void	meleeAttack(FragTrap const& target);
+
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+	void	beEnergized(unsigned int amount);
+
+	void	vaulthunter_dot_exe(FragTrap const& target);
 };

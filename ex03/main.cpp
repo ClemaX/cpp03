3184d6b262e7fc81160933bbfff6aea4ce0b6e41
Clément Hamada <clementhamada@protonmail.com>
2021-02-09 17:29:46 +0100

    ex03: Add new-lines in test

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,31 +4,49 @@
 
 int main(void)
 {
+	srand(time(NULL));
+
 	FragTrap a = FragTrap("Alpha");
 	FragTrap b = FragTrap("Beta");
 
+	std::cout << std::endl;
+
 	ScavTrap c = ScavTrap("Charlie");
 	ScavTrap d = ScavTrap("Delta");
+
+	std::cout << std::endl;
 
 	NinjaTrap e = NinjaTrap("Leonardo");
 	NinjaTrap f = NinjaTrap("Raphael");
 	NinjaTrap g = NinjaTrap("Donatello");
 
+	std::cout << std::endl;
+
 	a.meleeAttack(b);
 	b.rangedAttack(a);
+
+	std::cout << std::endl;
 
 	c.meleeAttack(d);
 	d.rangedAttack(c);
 
+	std::cout << std::endl;
+
 	for (std::size_t i = 0; i < 5; i++)
 		a.vaulthunter_dot_exe(b);
+
+	std::cout << std::endl;
 
 	for (std::size_t i = 0; i < 5; i++)
 		c.challengeNewcomer();
 
+	std::cout << std::endl;
+
 	e.ninjaShoebox(a);
 	f.ninjaShoebox(c);
 	g.ninjaShoebox(e);
+
+	std::cout << std::endl;
 
 	return 0;
 }

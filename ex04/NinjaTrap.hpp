87dab94289a61401a28f18b5bbf81223e810126f
Clément Hamada <clementhamada@protonmail.com>
2021-02-12 13:05:16 +0100

    Add symbolic links

--- ex04/NinjaTrap.hpp
+++ ex04/NinjaTrap.hpp
@@ -1,17 +1,1 @@
-#pragma once
-
-#include "ClapTrap.hpp"
-#include "FragTrap.hpp"
-#include "ScavTrap.hpp"
-
-class NinjaTrap : virtual public ClapTrap
-{
-public:
-	NinjaTrap(std::string name);
-	~NinjaTrap();
-
-	void performAttack(std::string &source, std::string const &target, ClapTrap::Attack const &attack);
-	void ninjaShoebox(FragTrap const &frag);
-	void ninjaShoebox(ScavTrap const &scav);
-	void ninjaShoebox(NinjaTrap const &ninja);
-};
+../ex03/NinjaTrap.hpp
